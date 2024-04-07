e4f0b3ba05367b4656497cda8f62f320b80890cb
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-04-07 23:02:40 +0900

    fix url

--- src/app/(pages)/blog/[id]/page.tsx
+++ src/app/(pages)/blog/[id]/page.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata= {
 }
 
 const getBlogArticle = async (id: string) => {
-  const res = await fetch('https://zenn-next-demo.vercel.app/api/blog'+id,{ cache: 'no-store' })
+  const res = await fetch('https://zenn-next-demo.vercel.app/api/blog/'+id,{ cache: 'no-store' })
 
   const blogArticle = await res.json()
 
